80343150a12555d4d520dd7e3d110e1c387efe3f
Millie-cyber458 <siriinsai@gmail.com>
2026-09-13 18:09:23 +0530

    made database, has some improvement to make

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template, jsonify
-
+import pymysql
+from config import DB_CONFIG
 app = Flask(__name__)
 @app.route('/')
+#helper functioin to fetch live classes from the database
 
-def home():  return render_template('home.html')
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
 
-    app.route('/Community')
+def get_live_classes():
+    conn = pymysql.connect(**DB_CONFIG)
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("SELECT * FROM LiveClass")
+    LiveClass = cursor.fetchall()
+    conn.close()
+    return LiveClass
 
-    app.route('/About')
-
-    @app.routr('/')
+    @app.route('/')
     def home():
+        LiveClass = get_live_class()
         return render_template('home.html', LiveClass=LiveClass)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+
