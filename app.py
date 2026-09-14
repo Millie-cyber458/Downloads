25ea4e00b12a11fbceea0a3fb559017c6f3aa668
Millie-cyber458 <siriinsai@gmail.com>
2026-09-14 18:03:17 +0530

    got the background database successfully integrated and my webpage running without error

--- app.py
+++ app.py
@@ -4,20 +4,14 @@ from config import DB_CONFIG
 app = Flask(__name__)
 @app.route('/')
 #helper functioin to fetch live classes from the database
-
-
-def get_live_classes():
+def home():
     conn = pymysql.connect(**DB_CONFIG)
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor.execute("SELECT * FROM LiveClass")
-    LiveClass = cursor.fetchall()
+    live_classes = cursor.fetchall()
     conn.close()
-    return LiveClass
+    return render_template('home.html', LiveClass=live_classes)
 
-    @app.route('/')
-    def home():
-        LiveClass = get_live_classes()
-        return render_template('home.html', LiveClass=LiveClass)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002, debug=True)
 
